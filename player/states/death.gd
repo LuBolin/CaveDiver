1bbba08f4275eded63627ef64130bc34230e21d3
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-04-19 18:20:13 +0800

    fadeout

--- player/states/death.gd
+++ player/states/death.gd
@@ -1,10 +1,13 @@
 extends State
 
+signal died
+
 func enter():
 	super()
 	parent.alive = false
 	$death.play()
-	parent.animations.animation_finished.connect(die)
+	#parent.animations.animation_finished.connect(die)
+	Singleton.lose.emit()
 
 func die():
 	Singleton.restart.emit()
